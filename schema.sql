d1fb0a2ff114be96abed176faa30382eb96993a0
unknown <blendikras96@gmail.com>
2023-02-28 15:54:33 +0100

    Schema

---
+++ schema.sql
@@ -0,0 +1,44 @@
+-- Creating tables for PH-EmployeeDB
+CREATE TABLE departments (
+     dept_no VARCHAR(4) NOT NULL,
+     dept_name VARCHAR(40) NOT NULL,
+     PRIMARY KEY (dept_no),
+     UNIQUE (dept_name)
+);
+
+-- Creating employees table
+create table employees(
+	emp_no int not null,
+	birth_date date not null,
+	first_name varchar not null,
+	last_name varchar not null,
+	gender varchar not null,
+	hire_dare date not null,
+	Primary key(emp_no)
+);
+
+
+CREATE TABLE dept_manager (
+dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+
+select * from departments
+
+
+
